1e4c870a802f37935c94f8017a7c17eae1be7747
Kazuki <kazukimatsudawork@gmail.com>
2021-05-02 18:10:56 +0900

    UserListItemComponentの分割

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { UserListItemComponent } from './user-list-item/user-list-item.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserListItemComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
